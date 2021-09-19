585e685d5922c6b8aee14cafc6d3195c5cb998d5
stoneroll6 <stoneroll6@gmail.com>
2021-09-19 20:26:30 +0900

    add start script

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ db.once('open', () => {
     console.log('Database connected!');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
